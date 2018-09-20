0793447c4736916e5873b2e2245dcd9e516a7a23
Toàn Võ <toanvolk@gmail.com>
2018-09-20 22:41:30 +0700

    add areas

---
+++ LuckyShop/Areas/Admin/AdminAreaRegistration.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+
+namespace LuckyShop.Areas.Admin
+{
+    public class AdminAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Admin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute("trangchu", "admin/trang-chu", new { controller = "Home", action = "Index" }, new string[] { "LuckyShop.Areas.Admin.Controllers" });
+            context.MapRoute(
+                "Admin_default",
+                "Admin/{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                new string[] { "LuckyShop.Areas.Admin.Controllers" }
+            );
+        }
+    }
+}
