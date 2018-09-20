7f6fe3a412d01354d360aa1400342199129a03a7
Toàn Võ <toanvolk@gmail.com>
2018-09-20 21:01:25 +0700

    Init

---
+++ LuckyShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LuckyShop.Startup))]
+namespace LuckyShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
